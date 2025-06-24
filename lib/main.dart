13f7fbbf0ded5475bd3dfc76b987339366c1f09f
ahmad-zhafir <zhafir2168@gmail.com>
2025-06-24 20:36:18 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  // ✅ Force sign out of Google and Firebase on every app restart
+  await GoogleSignIn().signOut();
+  await FirebaseAuth.instance.signOut();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Digital Certificate Repository',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+
+      },
+    );
+  }
+}
