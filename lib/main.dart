261bf2bade438b559185e08518a5cffb862ad353
HazriqHaykal <222309@student.upm.edu.my>
2025-06-25 15:55:47 +0800

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:certificate_gen/screens/admin/system_analytics_screen.dart';
+import 'package:certificate_gen/screens/admin/system_logs_stats_screen.dart';
+import 'package:certificate_gen/screens/ca/ca_true_copy_requests_screen.dart';
+import 'package:certificate_gen/screens/pdf_preview_screen.dart';
+import 'package:certificate_gen/screens/recipient/recipient_true_copy_upload_screen.dart';
+import 'package:certificate_gen/screens/verify_certificate_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'screens/login_screen.dart';
+import 'screens/dashboard_screen.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -26,6 +34,17 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
+        '/': (context) => LoginScreen(),
+        '/dashboard': (context) => DashboardScreen(),
+        '/admin-dashboard': (context) => const SystemAnalyticsScreen(),
+        '/admin-logs': (context) => SystemLogsAndStatsScreen(),
+        '/verify-certificate': (context) => const VerifyCertificateScreen(),
+        '/true-copy-upload': (context) => const RecipientTrueCopyUploadScreen(),
+        '/ca-true-copy-requests': (context) => const CaTrueCopyRequestsScreen(),
+        '/pdf-preview': (context) {
+          final url = ModalRoute.of(context)!.settings.arguments as String;
+          return PdfPreviewScreen(url: url);
+        },
 
       },
     );
